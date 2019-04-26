f71c5c43b201704fefbce130cfd82ebdb1c09704
Tristan Genevet <tristan@unity3d.com>
2019-04-26 17:36:18 +0200

    added error management when linking a GPU operator to a CPU slot.

--- com.unity.visualeffectgraph/Editor/GraphView/VFXViewWindow.cs
+++ com.unity.visualeffectgraph/Editor/GraphView/VFXViewWindow.cs
@@ -253,7 +253,12 @@ namespace  UnityEditor.VFX.UI
                         }
 
 
+                        var status = graph.compilationStatus;
                         graph.RecompileIfNeeded(!autoCompile,!autoCompileDependent);
+                        if( graph.compilationStatus != status)
+                        {
+                            graphView.UpdateStatus(graph.compilationStatus);
+                        }
                         controller.RecompileExpressionGraphIfNeeded();
                     }
                 }
